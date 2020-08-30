a5d2106335686b57d8ec468d6712b85f5212430d
Nikolay Ivanov <n_13@bk.ru>
2020-08-30 19:49:27 +0300

    deque: hw is done

--- Pascal/hw-deque/deque.pas
+++ Pascal/hw-deque/deque.pas
@@ -1,4 +1,4 @@
-program deque;
+program dequeList;
 
 type
     LongItem2Ptr = ^LongItem2;
@@ -17,7 +17,7 @@ begin
     deque.last := nil;
 end;
 
-procedure LongDequePushFront(var deque: LongDeque, n: longint);
+procedure LongDequePushFront(var deque: LongDeque; n: longint);
 var
     tmp: LongItem2Ptr;
 begin
@@ -40,38 +40,85 @@ begin
     deque.last := tmp;
 end;
 
-procedure LongDequePushBack(var deque: LongDeque, n: longint);
-var
-    tmp: LongItem2Ptr;
+procedure LongDequePushBack(var deque: LongDeque; n: longint);
 begin
-    new(tmp);
-    tmp^.data := n;
-
     if deque.first <> nil then
     begin
-        deque.first^.prev := tmp;
-        tmp^.next := deque.first;
-        tmp^.prev := nil;
+        new(deque.first^.prev);
+        deque.first^.prev^.next := deque.first;
+        deque.first := deque.first^.prev;
     end
     else
     begin
-        tmp^.prev := nil;
-        tmp^.next := nil;
-        deque.last := tmp;
+        new(deque.first);
+        deque.last := deque.first;
+        deque.first^.prev := nil;
+        deque.first^.next := nil;
     end;
 
-    deque.first := tmp;
+    deque.first^.data := n;
 end;
 
-function LongDequePopFront(var deque: LongDeque, var n: longint): boolean;
+function LongDequePopFront(var deque: LongDeque; var n: longint): boolean;
+var
+    tmp: LongItem2Ptr;
 begin
+    if (deque.first = nil) then
+    begin
+        LongDequePopFront := false;
+        exit
+    end;
+    
+    tmp := deque.first;
+    n := tmp^.data;
+    if tmp^.next <> nil then
+        tmp^.next^.prev := nil;
+    deque.first := tmp^.next;
+    dispose(tmp);
+    LongDequePopFront := true;
 end;
 
-function LongDequePopBack(var deque: LongDeque, var n: longint): boolean;
+function LongDequePopBack(var deque: LongDeque; var n: longint): boolean;
+var
+    tmp: LongItem2Ptr;
 begin
+    if deque.last = nil then
+    begin
+        LongDequePopBack := false;
+        exit
+    end;
+
+    tmp := deque.last;
+    n := tmp^.data;
+    if tmp^.prev <> nil then
+        tmp^.prev^.next := nil;
+    deque.last := tmp^.prev;
+    dispose(tmp);
+    LongDequePopBack := true;
 end;
 
 function LongDequeIsEmpty(var deque: LongDeque): boolean;
 begin
+    LongDequeIsEmpty := deque.first = nil;
 end;
 
+var
+    deque: LongDeque;
+    tmp: LongItem2Ptr;
+    num: longint;
+begin
+    LongDequeInit(deque);
+
+    while not SeekEof do
+    begin
+        readln(num);
+        LongDequePushBack(deque, num);
+    end;
+
+    writeln('POP: BEGIN');
+    while LongDequePopFront(deque, num) do
+    begin
+        writeln(num);
+    end;
+    writeln('POP: END;');
+end.
